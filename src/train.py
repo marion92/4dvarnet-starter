e0a1cdcb731f7351e358a1cd8a56877c33dc49e3
Marion POUSSIN <marionpoussin92@gmail.com>
2023-07-19 15:25:02 +0200

    debug

--- src/train.py
+++ src/train.py
@@ -8,9 +8,8 @@ def base_training(trainer, dm, lit_mod, ckpt=None):
         print("Logdir:", trainer.logger.log_dir)
         print()
 
-    ckpt = './val_mse=0.00055-epoch=136.ckpt'
-    #trainer.fit(lit_mod, datamodule=dm, ckpt_path=ckpt)
-    trainer.test(lit_mod, datamodule=dm, ckpt_path=ckpt)
+    trainer.fit(lit_mod, datamodule=dm, ckpt_path=ckpt)
+    trainer.test(lit_mod, datamodule=dm, ckpt_path='best')
 
 def multi_dm_training(trainer, dm, lit_mod, test_dm=None, test_fn=None, ckpt=None):
     print('multi_dm_training (train)')
